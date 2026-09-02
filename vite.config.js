26ec548deceb595b939d15161ce0cb2a93c50823
borseswapnilb-web <borseswapnilb@gmail.com>
2026-09-02 20:53:33 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   const base = env.VITE_BASE_PATH || '/'
   return {
-    base'/portfolio/',
+    base: '/portfolio/',
     plugins: [react()],
   }
 })
